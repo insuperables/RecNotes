247999940dca88d94e1460100146a737ecc9009d
insuperables <insuperable291@gmail.com>
2020-12-25 16:54:00 +0530

    First Step to make App

---
+++ app/src/main/java/com/recnotes/MainActivity.java
@@ -0,0 +1,71 @@
+package com.recnotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    private String webUrl = "https://recnotes.com/";
+
+    ProgressBar progressBarWeb;
+    ProgressDialog progressDialog;
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.myWebView);
+        progressBarWeb=(ProgressBar)findViewById(R.id.progressBar);
+        progressDialog=new ProgressDialog(this);
+        progressDialog.setMessage("Loading Please Wait");
+
+        webView.loadUrl(webUrl);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient(){
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+
+                progressBarWeb.setVisibility(View.VISIBLE);
+                progressBarWeb.setProgress(newProgress);
+                setTitle("Loading...");
+                progressDialog.show();
+                if(newProgress == 100){
+                    progressBarWeb.setVisibility(View.GONE);
+                    setTitle(view.getTitle());
+                    progressDialog.dismiss();
+                }
+                super.onProgressChanged(view, newProgress);
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else
+            super.onBackPressed();
+    }
+}
